36af9883c936312d4920f1e7e3d69937083109dd
Rene Nejsum <renejsum@macbook.mynet>
2015-06-06 18:26:45 +0200

    Example cleanup

--- conf/usertasks.py
+++ conf/usertasks.py
@@ -1,21 +1,21 @@
-from example.worker.worker import WorkerTask
+from example.worker import WorkerTask
 conf.addTask( "worker", WorkerTask )
 
-from example.client.client import ClientTask
+from example.client import ClientTask
 
-conf.addTask( "client1", ClientTask, conf="client/client.conf" )
-# conf.addTask( "client2", ClientTask, conf="client/client.conf" )
-# conf.addTask( "client3", ClientTask, conf="client/client.conf" )
+conf.addTask( "client1", ClientTask, conf="example/conf/client.py" )
+conf.addTask( "client2", ClientTask, conf="example/conf/client.py" )
+conf.addTask( "client3", ClientTask, conf="example/conf/client.py" )
 
-from example.statetask.statetask import StateTask
+from example.statetask import StateTask
 conf.addTask( "stateclient", StateTask )
 
-from example.netserver.netserver import EchoServerTask
+from example.netserver import EchoServerTask
 conf.addTask( "netserver", EchoServerTask )
 
-from example.netclient.netclient import EchoClientTask
+from example.netclient import EchoClientTask
 conf.addTask( "netclient", EchoClientTask )
 
-# from ring.ring import RingTask
-# for r in range( 100 ):
-#     conf.addTask( "ring%d" % r, RingTask, index=r )
+from example.ring import RingTask
+for r in range( 100 ):
+    conf.addTask( "ring%d" % r, RingTask, index=r, conf="example/conf/ring.py" )
--- example/conf/client.py
+++ example/conf/client.py
@@ -1,3 +1,4 @@
 task.log( "in client conf: %s" % task.get_name( ))
 if task.get_name( ) == "client2" :
-   task.setNtimeout( 1 )
+    task.setNtimeout( 1 )
+    
