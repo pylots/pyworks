0b9f529240902f46d00d3914bae421baae24ae01
Rene Nejsum <renejsum@macbook.mynet>
2015-06-06 18:07:23 +0200

    Added example filder

--- conf/usertasks.py
+++ conf/usertasks.py
@@ -1,19 +1,19 @@
-from worker.worker import WorkerTask
+from example.worker.worker import WorkerTask
 conf.addTask( "worker", WorkerTask )
 
-from client.client import ClientTask
+from example.client.client import ClientTask
 
 conf.addTask( "client1", ClientTask, conf="client/client.conf" )
 # conf.addTask( "client2", ClientTask, conf="client/client.conf" )
 # conf.addTask( "client3", ClientTask, conf="client/client.conf" )
 
-from statetask.statetask import StateTask
+from example.statetask.statetask import StateTask
 conf.addTask( "stateclient", StateTask )
 
-from netserver.netserver import EchoServerTask
+from example.netserver.netserver import EchoServerTask
 conf.addTask( "netserver", EchoServerTask )
 
-from netclient.netclient import EchoClientTask
+from example.netclient.netclient import EchoClientTask
 conf.addTask( "netclient", EchoClientTask )
 
 # from ring.ring import RingTask
